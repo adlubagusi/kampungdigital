b39b539cca74a28e5b4f77362ac42210c4c9f5d4
Fajar Budi <46071049+Jarbud@users.noreply.github.com>
2020-10-29 19:30:49 +0700

    update halaman surat frontend

--- application/helpers/my_helper.php
+++ application/helpers/my_helper.php
@@ -16,12 +16,13 @@ function gen_menu()
 {
     $CI 	=& get_instance();
     $url = $CI->uri->segment(1);
-    
+
     $menu = array(
         array("icon"=>"", "url"=>"", "text"=>"Home"),
         array("icon"=>"", "url"=>"about", "text"=>"Tentang Kami"),
         array("icon"=>"", "url"=>"blog", "text"=>"Blog"),
-        array("icon"=>"", "url"=>"contact", "text"=>"Kontak")
+        array("icon"=>"", "url"=>"contact", "text"=>"Kontak"),
+				array("icon"=>"", "url"=>"letter", "text"=>"Surat")
       );
 
     if ($menu != null) {
@@ -37,39 +38,39 @@ function gen_menu()
                                   <a class="nav-link" href="'.base_url().''.$m['url'].'">'.$m['text'].'</a>
                               </li>';
                   }
-                } 
+                }
           echo'
             </ul>
-          ' ; 
+          ' ;
     }
 }
 
 function gen_menu_admin(){
   $CI 	=& get_instance();
   $url = $CI->uri->segment(2);
-  
+
   $menu = array(
       ["icon"=>"fa-home", "url"=>"dashboard", "text"=>"Dashboard"], //menu biasa
       ["icon"=>"fa-users", "url"=>"pengguna", "text"=>"Pengguna"],
-      ["icon"=>"fa-envelope", "url"=>"suratmasuk", "text"=>"Surat Masuk", "submenu"=>[            
-        ["icon"=>"fa-envelope-o", "url"=>"suratmasuk-list", "text"=>"List Surat Masuk"]  
-      ]],                                                                       
-      ["icon"=>"fa-newspaper-o", "url"=>"blog", "text"=>"Blog", "submenu"=>[      //menu spesial    
+      ["icon"=>"fa-envelope", "url"=>"suratmasuk", "text"=>"Surat Masuk", "submenu"=>[
+        ["icon"=>"fa-envelope-o", "url"=>"suratmasuk-list", "text"=>"List Surat Masuk"]
+      ]],
+      ["icon"=>"fa-newspaper-o", "url"=>"blog", "text"=>"Blog", "submenu"=>[      //menu spesial
           ["icon"=>"fa-list", "url"=>"blog", "text"=>"List Postingan"], //
           ["icon"=>"fa-wrench", "url"=>"blog-kategori", "text"=>"Kategori"] //
       ]],                                                                    //
-      ["icon"=>"fa-gear", "url"=>"setting", "text"=>"Pengaturan", "submenu"=>[            
-          ["icon"=>"fa-file-o", "url"=>"setting-general", "text"=>"Pengaturan Umum"], 
-          ["icon"=>"fa-file-o", "url"=>"setting-about", "text"=>"Teks About Us"]  
-      ]],                                                                       
+      ["icon"=>"fa-gear", "url"=>"setting", "text"=>"Pengaturan", "submenu"=>[
+          ["icon"=>"fa-file-o", "url"=>"setting-general", "text"=>"Pengaturan Umum"],
+          ["icon"=>"fa-file-o", "url"=>"setting-about", "text"=>"Teks About Us"]
+      ]],
       ["icon"=>"fa-sign-out", "url"=>"logout", "text"=>"Logout"],
   );
-  
-  
+
+
   $vaMenu = $menu;
   foreach($vaMenu as $k=>$m){
     $cUrl  = "#";
-    
+
     $cActive = "";
     if($m['url'] <> "") $cUrl = base_url()."admin/".$m['url'];
     $cIcon = $m['icon'];
@@ -80,11 +81,11 @@ function gen_menu_admin(){
     if($url == $m['url'] || $vaUrl[0] == $m['url']) $cActive = "active";
     echo '<li class="'.$cActive.'">
             <a href="'.$cUrl.'">
-            <i class="fa '.$cIcon.'"></i> 
+            <i class="fa '.$cIcon.'"></i>
             <span>'.$cText.'</span>';
     echo '  <span class="pull-right-container">
               '.$cIconRight.'
-            </span>';                  
+            </span>';
     echo '  </a>';
     if(isset($m['submenu'])){
       $vaSubmenu = $m['submenu'];
@@ -93,7 +94,7 @@ function gen_menu_admin(){
         echo '<li><a href="'.base_url().'admin/'.$sm['url'].'"><i class="fa '.$sm['icon'].'"></i> '.$sm['text'].'</a></li>';
       }
       echo '</ul>';
-    }      
+    }
     echo '</li>';
   }
 }
@@ -112,11 +113,11 @@ function saveCfg($cKode,$cKeterangan){
   $CI 	=& get_instance();
   $dbData = $CI->db->query("SELECT * FROM tbl_config WHERE Kode='$cKode'");
   if($dbRow = $dbData->row()){
-    $dbd  = $CI->db->query("UPDATE tbl_config SET Keterangan='$cKeterangan' WHERE Kode='$cKode'"); 
+    $dbd  = $CI->db->query("UPDATE tbl_config SET Keterangan='$cKeterangan' WHERE Kode='$cKode'");
   }else{
-    $dbd  = $CI->db->query("INSERT INTO tbl_config (Kode, Keterangan) VALUES('$cKode','$cKeterangan')"); 
+    $dbd  = $CI->db->query("INSERT INTO tbl_config (Kode, Keterangan) VALUES('$cKode','$cKeterangan')");
   }
-   
+
 }
 
 function getVal($cKode,$cField,$cTable,$cWhereField="Kode"){
@@ -151,5 +152,5 @@ function formatSizeUnits($bytes){
   }else{
       $bytes = '0 bytes';
   }
-  return $bytes;  
+  return $bytes;
 }
