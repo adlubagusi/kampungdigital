6dcc2e011db254650250638cb62c04a51fac0f55
Adlu Bagus Irawan <adlubagusi@gmail.com>
2020-11-15 14:07:57 +0800

    Penambahan modul CRUD bidang usaha

---
+++ application/views/admin/bidangusaha/v_bidangusaha-js.php
@@ -0,0 +1,202 @@
+<script>
+
+    $(function () {
+
+        $(".select2").select2();
+        CKEDITOR.replace('cDeskripsi');
+        pagination("datatabel", base_url+"/admin/bidangusaha/getdata", []);
+    });
+
+    $('#f_bidangusaha').submit(function(e){
+        e.preventDefault();
+        
+        var table = $('#datatabel').DataTable();
+        //untuk ambil value dari ckeditor
+        var cDeskripsi = CKEDITOR.instances.cDeskripsi.getData();
+        
+        var fd = new FormData(this);
+        fd.append('cDeskripsi',cDeskripsi);
+        $.ajax({
+            url:'<?php echo base_url();?>/admin/bidangusaha/save',
+            type:"post",
+            data:fd,
+            processData:false,
+            contentType:false,
+            cache:false,
+            async:false,
+            success: function(reply){
+                if(reply == "ok"){
+                    Swal.fire({
+                        icon: "success",
+                        title: "Data Disimpan!",
+                    });  
+                    $("#modalBidangUsaha").modal('hide');
+                    table.ajax.reload();
+                }
+        }
+        });
+    });
+
+    $('#f_submit_hapus').submit(function(e){
+        var table = $('#datatabel').DataTable();
+        e.preventDefault(); 
+        $.ajax({
+            url:'<?php echo base_url();?>/admin/bidangusaha/delete',
+            type:"post",
+            data:new FormData(this),
+            processData:false,
+            contentType:false,
+            cache:false,
+            async:false,
+            success: function(data){
+                $("#modalHapus").modal('hide');
+                table.ajax.reload();
+        }
+        });
+    }); 
+
+    function bidangusaha_edit(id) {
+        $("#nID").val("");
+        $("#cJudul").val("");
+        $("#cSlug").val("");
+        CKEDITOR.instances['cDeskripsi'].setData("");
+        $.ajax({
+            type: "GET",
+            url: base_url+"admin/bidangusaha/detail/"+id,
+            success: function(data) {           
+                loadFileBidangUsaha(data.File)
+                $("#cKode").val(data.Kode);
+                $("#cNamaOwner").val(data.NamaOwner);
+                $("#cNamaUsaha").val(data.NamaUsaha);
+                $("#cAlamatUsaha").val(data.AlamatUsaha);
+                $("#cHP").val(data.HP);
+                CKEDITOR.instances['cDeskripsi'].setData(data.Deskripsi);
+                $("#optJenisUsaha").val(data.JenisUsaha);
+                $("#vaFile").val("");
+            }
+        });
+        $("#modalBidangUsaha").modal('show');
+        return false;
+    }
+
+    function bidangusaha_hapus(id){
+        $("#modalHapus").modal('show');
+            $.ajax({
+            type: "GET",
+            url: base_url+"admin/bidangusaha/detail/"+id,
+            success: function(data) {
+                console.log(data);            
+                $("#cKodeHapus").val(data.Kode);
+                $("#cNamaOwnerHapus").html(data.NamaOwner);
+            }
+        });
+    }
+    function bidangusaha_show(id){
+        $("#modalBidangUsahaShow").modal('show');
+        $.ajax({
+            type: "GET",
+            url: base_url+"admin/bidangusaha/detail/"+id,
+            success: function(data) {
+                console.log(data);
+                $("#nIDShow").val(data.ID);
+                $("#cJudulShow").val(data.Judul);
+                $("#cDeskripsiShow").html(data.Deskripsi)
+                $("#optKategoriShow").html(data.KeteranganKategori);
+
+                //disable input
+                $("#nIDShow").prop('disabled', true);
+                $("#cJudulShow").prop('disabled', true);
+                $("#imgShow").attr('src', base_url+"assets/images/bidangusaha/"+data.Image);
+            }
+        });
+        return false;
+    }
+
+    $("#vaFile").on('change', function(e){
+        e.preventDefault();
+        var name   = $(this).attr("id");
+        var file   = $(this)[0].files;
+        var fdfile = new FormData();
+        for(var i = 0; i < file.length; i++){
+            fdfile.append("vaFile[]",file[i]);
+        }
+        $.ajax({
+            url:'<?php echo base_url();?>/admin/bidangusaha/savingFile',
+            type:"post",
+            data:fdfile,
+            processData:false,
+            contentType:false,
+            cache:false,
+            async:false,
+            success: function(data){
+                return false;
+        }
+        });
+    })
+
+    function loadFileBidangUsaha(file){
+        $("#listFile").css("display","none");
+        $("#areaFileBidangUsaha").html("");
+        for(var i=0; i<file.length;i++){
+            var id           = file[i].ID;
+            var cFileName    = file[i].FileName;
+            var cFileNameCut = cFileName.substring(0,20);
+            var cFilePath    = file[i].FilePath;
+            var cFileSize    = file[i].FileSize;
+
+            var $liFile           = $('<li class="itemFile"></li>');
+            var $spanIcon         = $('<span class="mailbox-attachment-icon"><i class="fa fa-file-text"></i></span>');
+            var $aDeleteFile      = $('<a href="#" onclick="deleteFile('+id+')" style="float: right;margin: 5px;color: red;font-size:20px;" title="Hapus File?"><i class="fa fa-trash"></i></a>');
+            var $divFileInfo      = $('<div class="mailbox-attachment-info"></div>');
+            var $aLinkFile        = $('<a href="'+base_url+cFilePath+'" class="mailbox-attachment-name" title="'+cFileName+'" target="_blank"><i class="fa fa-paperclip"></i>&nbsp;'+cFileNameCut+'</a>');
+            var $spanDownloadFile = $('<span class="mailbox-attachment-size">'+cFileSize+'</span>');
+            var $aLinkDownload    = $('<a href="'+base_url+cFilePath+'" class="btn btn-default btn-xs pull-right" title="Download" download><i class="fa fa-cloud-download"></i></a>');
+            
+            $spanDownloadFile.append($aLinkDownload);
+            $divFileInfo.append($aLinkFile);
+            $divFileInfo.append($spanDownloadFile);
+            $liFile.append($aDeleteFile);
+            $liFile.append($spanIcon);
+            $liFile.append($divFileInfo);
+
+            $("#areaFileBidangUsaha").append($liFile);
+        } 
+        if(file.length > 0) $("#listFile").css("display","block");
+    }
+
+    function deleteFile(id){
+        Swal.fire({
+            title: "Apakah Anda Yakin?",
+            text: "Anda akan menghapus file ini",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, Saya yakin!',
+            cancelButtonText: 'Batal!'
+        }).then((result) => {
+            if (result.value) {
+                
+                //ajax mulai disini
+                var fd = new FormData();
+                fd.append('cID', id)
+                $.ajax({
+                    url:'<?php echo base_url();?>/admin/bidangusaha/deleteFile',
+                    type:"post",
+                    data:fd,
+                    processData:false,
+                    contentType:false,
+                    cache:false,
+                    async:false,
+                    success: function(data){
+                        Swal.fire({
+                            icon: "success",
+                            title: "File Dihapus!",
+                        });  
+                        $("#modalBidangUsaha").modal('hide');
+                }
+                });
+            }
+        });
+    }
+</script>
---
+++ application/views/admin/bidangusaha/v_bidangusaha.php
@@ -0,0 +1,154 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+      <h1>
+        Data Bidang Usaha
+        <small></small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Bidang Usaha</a></li>
+        <li class="active">Data Bidang Usaha</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+
+          <div class="box">
+            <div class="box-header">
+              <a class="btn btn-success btn-flat" onclick="bidangusaha_edit(0)"><span class="fa fa-user-plus"></span> Tambah Bidang Usaha</a>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+            <table class="table table-striped" id="datatabel" style="font-size:13px;">
+                <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama Owner</th>
+                    <th>Nama Usaha</th>
+                    <th>No. HP</th>
+                    <th>Jenis Usaha</th>
+                    <th style="text-align:center;">Aksi</th>
+                </tr>
+                </thead>
+
+                <tbody></tbody>
+            </table>
+
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+
+    
+
+    <div class="modal fade" id="modalBidangUsaha" tabindex="-1" role="dialog" aria-labelledby="modalBidangUsahaLabel">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><span class="fa fa-close"></span></span></button>
+                    <h4 class="modal-title" id="modalBidangUsahaLabel">Add/Edit Bidang Usaha</h4>
+                </div>
+                <form class="form-horizontal" method="post" id="f_bidangusaha">
+                <div class="modal-body">
+                    <input type="hidden" id="nID" name="nID" value="0">
+                    <input type="hidden" id="cTipe">
+                    <input type="hidden" name="cKode" class="form-control" id="cKode">
+                    <div class="form-group">
+                        <label for="cNamaOwner" class="col-sm-2 control-label">Nama Owner</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="cNamaOwner" class="form-control" id="cNamaOwner" placeholder="Nama Owner" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="cNamaUsaha" class="col-sm-2 control-label">Nama Usaha</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="cNamaUsaha" class="form-control" id="cNamaUsaha" placeholder="Nama Usaha" required>
+                        </div>
+                    </div><div class="form-group">
+                        <label for="cAlamatUsaha" class="col-sm-2 control-label">Alamat Usaha</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="cAlamatUsaha" class="form-control" id="cAlamatUsaha" placeholder="Alamat Usaha" required>
+                        </div>
+                    </div><div class="form-group">
+                        <label for="cHP" class="col-sm-2 control-label">No. HP</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="cHP" class="form-control" id="cHP" placeholder="No. HP" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="cDeskripsi" class="col-sm-2 control-label">Deskripsi</label>
+                        <div class="col-sm-10">
+                            <textarea id="cDeskripsi" name="cDeskripsi" required rows="20" cols="5"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="optJenisUsaha" class="col-sm-2 control-label">Jenis Usaha</label>
+                        <div class="col-sm-10">
+                            <select id="optJenisUsaha" class="form-control" style="width: 100%;" name="optJenisUsaha" required >
+                                <option value="">-Pilih-</option>
+                                <?php
+                                    foreach($vaJenisUsaha as $key=>$val){
+                                ?>
+                                    
+                                    <option value="<?=$val['Kode']?>"> <?=$val['Keterangan']?> </option>
+                                <?php 
+                                    } 
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group" id="listFile" style="display:none;">
+                        <label for="" class="col-sm-2 control-label">Daftar File</label>
+                        <div class="col-sm-10">
+                            <ul class="mailbox-attachments clearfix" id="areaFileBidangUsaha">
+                                
+                            </ul>
+                        </div>                        
+                    </div>    
+                    <div class="form-group">
+                        <label for="vaFile" class="col-sm-2 control-label">File</label>
+                        <div class="col-sm-10">
+                            <input type="file" name="vaFile[]" id="vaFile" multiple class="form-control"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary btn-flat" id="simpan">Simpan</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <!--Modal Hapus Bidang Usaha-->
+    <div class="modal fade" id="modalHapus" tabindex="-1" role="dialog" aria-labelledby="modalHapusLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><span class="fa fa-close"></span></span></button>
+                    <h4 class="modal-title" id="ModalHapusLabel">Hapus Pengguna</h4>
+                </div>
+                <form class="form-horizontal" method="post" id="f_submit_hapus">
+                <div class="modal-body">
+                        <input type="hidden" id="cKodeHapus" name="cKodeHapus" value="0"/>
+                        <p>Apakah Anda yakin mau menghapus Bidang Usaha milik <b><span id="cNamaOwnerHapus"></span></b> ?</p>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary btn-flat" id="hapus">Hapus</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
